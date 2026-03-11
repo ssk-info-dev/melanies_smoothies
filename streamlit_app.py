62fe11379e3f9a5866b0c26ad7c5925e6b69e5d4
ssk-info-dev <nico.nelissen@ssk.co.za>
2026-03-11 10:16:52 +0000

    Code changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,20 +39,13 @@ if ingredients_list:
 
         ingredients_string += fruit_chosen + ' '
 
+        st.subheader(fruit_chosen + ' Nutrition Information')
+
         smoothiefroot_response = requests.get(
-            "https://my.smoothiefroot.com/api/fruit/watermelon"
+            "https://my.smoothiefroot.com/api/fruit/" + fruit_chosen
         )
 
-        st.dataframe(
+        sf_df = st.dataframe(
             data=smoothiefroot_response.json(),
             use_container_width=True
         )
-
-    my_insert_stmt = """insert into smoothies.public.orders(ingredients, name_on_order)
-                        values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
-
-    time_to_insert = st.button('Submit Order')
-
-    if time_to_insert:
-        session.sql(my_insert_stmt).collect()
-        st.success('Your Smoothie is ordered!', icon="✅")
