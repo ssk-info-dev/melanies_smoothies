dd0fbe48aa28f3199458b73d5ada5f68e08aa8b7
ssk-info-dev <nico.nelissen@ssk.co.za>
2026-03-11 11:26:11 +0200

    Update Snowflake session connection method
    Connected changes needed bc of move from SiS to SniS

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -14,7 +13,9 @@ Choose the fruits you want in your custom Smoothie!
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your Smoothie will be:", name_on_order)
 
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
+
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 
 fruit_rows = my_dataframe.collect()
