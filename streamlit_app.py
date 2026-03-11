74ae7810148a2a88a92c179e753e1f56ef4ccb5f
ssk-info-dev <nico.nelissen@ssk.co.za>
2026-03-11 09:38:21 +0000

    Title changed

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 
-st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
+st.title("My parents New Healthy Diner")
 
 st.write(
 """
