64a2c15f18f089e561499bd29635db07f68540ba
ssk-info-dev <nico.nelissen@ssk.co.za>
2026-03-11 10:24:43 +0000

    Added new data frame

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,11 @@ st.write("The name on your Smoothie will be:", name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options") \
+    .select(col("FRUIT_NAME"), col("SEARCH_ON"))
+
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 fruit_rows = my_dataframe.collect()
 fruit_list = [row['FRUIT_NAME'] for row in fruit_rows]
