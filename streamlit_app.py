9169d1f435fe317cf72aad8a0db6b6ca6dd81dd8
ssk-info-dev <nico.nelissen@ssk.co.za>
2026-03-11 10:03:03 +0000

    Fixed URL

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests  
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-st.text(smoothiefroot_response)
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 
