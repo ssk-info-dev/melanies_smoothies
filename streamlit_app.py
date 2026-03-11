74fc559b24c30a175c55b9f4bc774a90c5b1a80f
ssk-info-dev <nico.nelissen@ssk.co.za>
2026-03-11 10:34:43 +0000

    st.stop removed

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,16 @@
-# Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-import requests  
+import requests
 import pandas as pd
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json)
+st.write(smoothiefroot_response.json())
 
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 
-st.write(
-"""
+st.write("""
 Choose the fruits you want in your custom Smoothie!
-"""
-)
+""")
 
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your Smoothie will be:", name_on_order)
@@ -21,41 +18,39 @@ st.write("The name on your Smoothie will be:", name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(
+    col("FRUIT_NAME"), col("SEARCH_ON")
+)
 
 pd_df = my_dataframe.to_pandas()
 
-st.dataframe(data=my_dataframe, use_container_width=True)
-st.stop()
-
-fruit_rows = my_dataframe.collect()
-fruit_list = [row['FRUIT_NAME'] for row in fruit_rows]
+st.dataframe(data=pd_df, use_container_width=True)
 
 ingredients_list = st.multiselect(
-    'Choose up to 5 ingredients:',
-    pd_df['FRUIT_NAME'],
+    "Choose up to 5 ingredients:",
+    pd_df["FRUIT_NAME"].tolist(),
     max_selections=5
 )
 
 if ingredients_list:
-
-    ingredients_string = ''
+    ingredients_string = ""
 
     for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + " "
 
-        ingredients_string += fruit_chosen + ' '
+        search_on = pd_df.loc[
+            pd_df["FRUIT_NAME"] == fruit_chosen, "SEARCH_ON"
+        ].iloc[0]
 
-        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write("The search value for", fruit_chosen, "is", search_on)
 
-        st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
-
-        st.subheader(fruit_chosen + ' Nutrition Information')
+        st.subheader(f"{fruit_chosen} Nutrition Information")
 
         smoothiefroot_response = requests.get(
             "https://my.smoothiefroot.com/api/fruit/" + search_on
         )
 
         st.dataframe(
-            data=smoothiefroot_response.json(),
+            data=pd.DataFrame([smoothiefroot_response.json()]),
             use_container_width=True
         )
